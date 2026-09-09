5e0d35697e1ca000ffe7299c8d989bd4ce762b48
Jayasree <jayasreejsp630@gmail.com>
2026-09-09 11:46:25 +0530

    chore: initial project scaffold
    - pnpm + TypeScript 5 + Vitest 2 + ESLint 9 + Prettier 3
    - GitHub Actions CI running lint, typecheck, test on push to main
    - Flat-config ESLint with Node and ES2022 globals

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+ eslint.configs.recommended,
+ {
+ files: ["**/*.ts"],
+ languageOptions: {
+ parser: tsparser,
+ parserOptions: {
+ ecmaVersion: 2022,
+ sourceType: "module",
+ },
+ globals: {
+ ...globals.node,
+ ...globals.es2022,
+ },
+ },
+ plugins: {
+ "@typescript-eslint": tseslint,
+ },
+ rules: {
+ ...tseslint.configs["recommended"].rules,
+ "@typescript-eslint/no-explicit-any": "off",
+ "@typescript-eslint/explicit-module-boundary-types": "off",
+ "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+ "no-undef": "off",
+ },
+ },
+];
